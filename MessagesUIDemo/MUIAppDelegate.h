b160f065fc6a79dee48bf043362d157f31202c2a
Atsushi NAGASE <atsn.ngs@gmail.com>
2012-07-03 04:22:17 +0900

    Initial Commit

---
+++ MessagesUIDemo/MUIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MUIAppDelegate.h
+//  MessagesUIDemo
+//
+//  Created by Atsushi Nagase on 7/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MUIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MessagesUIDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MessagesUIDemo
+//
+//  Created by Atsushi Nagase on 7/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MUIAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([MUIAppDelegate class]));
+  }
+}
